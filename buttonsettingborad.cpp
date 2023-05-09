d75fc3d23270a2661aee79c759f89be1a391c3ed
ChaoII <849453582@qq.com>
2023-05-09 11:15:53 +0800

    optimize ui style

---
+++ buttonsettingborad.cpp
@@ -0,0 +1,141 @@
+#include "buttonsettingborad.h"
+#include <QSpacerItem>
+
+ButtonSettingPair::ButtonSettingPair(
+        QWidget *parent, const QString &labelText, const QString &prompt)
+    : QHBoxLayout(parent), label_text(labelText), prompt(prompt) {
+    delete_button = new QPushButton("-");
+    delete_button->setMaximumSize(25, 25);
+    this->addWidget(delete_button);
+    name = new QLineEdit(parent);
+    name->setText(labelText);
+    name->setMaximumWidth(50);
+    this->addWidget(name);
+    content = new QLineEdit(parent);
+    content->setText(prompt);
+    this->addWidget(content);
+    this->addSpacing(10);
+
+}
+
+ButtonSettingPair::~ButtonSettingPair() {
+    if (content != nullptr) {
+        delete content;
+        content = nullptr;
+    }
+    if (name != nullptr) {
+        delete name;
+        name = nullptr;
+    }
+    if (delete_button != nullptr) {
+        delete delete_button;
+        delete_button = nullptr;
+    }
+}
+
+ButtonSettingBorad::ButtonSettingBorad(QWidget *parent) : QWidget(parent) {
+    QVBoxLayout* v_layout = new QVBoxLayout;
+    box = new QVBoxLayout;
+
+    add_button = new QPushButton("添加prompt", parent);
+    this->setLayout(v_layout);
+    v_layout->addLayout(box);
+    QSpacerItem * sparcer_item = new QSpacerItem(0,160,QSizePolicy::Fixed,QSizePolicy::Expanding);
+    v_layout->addSpacerItem(sparcer_item);
+    v_layout->addWidget(add_button);
+}
+
+ButtonSettingBorad::~ButtonSettingBorad() {
+}
+
+void ButtonSettingBorad::ReloadSetting(ButtonList *list) {
+    if (list == nullptr) {
+        return;
+    }
+    QLayoutItem *child;
+    while ((child = box->takeAt(0)) != nullptr) {
+        delete child->widget();
+        delete child;
+    }
+    for (auto &item : list->list_buttons) {
+        auto *line = new ButtonSettingPair(nullptr, item.name, item.prompt);
+        connect(line->delete_button, &QPushButton::clicked,
+                [name = item.name, prompt = item.prompt,
+                this](bool clicked = false) { emit DeletePrompt(name, prompt); });
+        connect(line->name, &QLineEdit::textChanged,
+                [&item](const QString text) { item.name = text; });
+        connect(line->content, &QLineEdit::textChanged,
+                [&item](const QString text) { item.prompt = text; });
+        box->addLayout(line);
+    }
+}
+
+ButtonNode::ButtonNode(const QString &prompt, const QString &name,
+                       QWidget *parent)
+    : prompt(prompt), name(name) {
+    button = new QPushButton(name, parent);
+}
+
+ButtonNode::ButtonNode(const QString &prompt, const QString &name)
+    : prompt(prompt), name(name) {
+    button = nullptr;
+}
+
+void ButtonList::SaveToSettings(QSettings *settings) {
+    int cnt = 0;
+    int last_cnt = settings->value("btns").toInt();
+    for (int i = 0; i < list_buttons.size(); ++i) {
+        auto &btn = list_buttons[i];
+        if (btn.name == "名称" || btn.prompt == "提示语") {
+            qDebug() << "存在无效按钮";
+            cnt++;
+            continue;
+        }
+        {
+            QString key_name = QString("btn.prompt.%1").arg(i - cnt);
+            settings->setValue(key_name, btn.prompt);
+        }
+        {
+            QString key_name = QString("btn.name.%1").arg(i - cnt);
+            settings->setValue(key_name, btn.name);
+        }
+    }
+    settings->setValue("btns", list_buttons.size() - cnt);
+    qDebug() << "cnts:" << cnt << ",last : " << last_cnt
+             << " , all : " << list_buttons.size();
+    for (int i = list_buttons.size() - cnt;
+         i < list_buttons.size() || i < last_cnt; ++i) {
+        {
+            QString key_name = QString("btn.prompt.%1").arg(i);
+            settings->remove(key_name);
+        }
+        {
+            QString key_name = QString("btn.name.%1").arg(i);
+            settings->remove(key_name);
+        }
+    }
+}
+
+void ButtonList::ReadFromSettings(QSettings *settings) {
+    auto cnts = settings->value("btns", "-1").toInt();
+    if (cnts == -1) {
+        list_buttons.push_back(ButtonNode("请你用中文帮我解释该代码:", "解释"));
+        list_buttons.push_back(ButtonNode(
+                                   "请你将以下内容翻译为中文，其中术语可能偏向于计算机行业:", "翻译"));
+        list_buttons.push_back(
+                    ButtonNode("请你用中文，就以下内容进行创作:", "创作"));
+        list_buttons.push_back(ButtonNode("请你用中文，总结以下内容:", "归纳"));
+        SaveToSettings(settings);
+    } else {
+        list_buttons.clear();
+        for (int i = 0; i < cnts; i++) {
+            QString prompt_key = QString("btn.prompt.%1").arg(i);
+            QString name_key = QString("btn.name.%1").arg(i);
+            QString prompt = settings->value(prompt_key, "").toString();
+            QString name = settings->value(name_key, "").toString();
+            if (!prompt.isEmpty() && !name.isEmpty()) {
+                list_buttons.push_back(ButtonNode(prompt, name));
+            }
+        }
+    }
+}
--- utils/settingsdialog.h
+++ utils/settingsdialog.h
@@ -2,57 +2,22 @@
 #define SETTINGSDIALOG_H
 
 #include <QDialog>
-#include <QSettings>
 #include <map>
 #include <QString>
 #include <QLineEdit>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QLabel>
+#include "buttonsettingborad.h"
+
+
+
+
 namespace Ui {
 class SettingsDialog;
 }
-class ButtonNode{
-public:
-    QPushButton * button;
-    QString prompt;
-    QString name;
-public:
-    ButtonNode(const QString &prompt, const QString &name , QWidget *parent);
-    ButtonNode(const QString &prompt, const QString &name );
-};
-class ButtonList : QHBoxLayout{
-public:
-    std::vector<ButtonNode> list_buttons;
-    void SaveToSettings(QSettings * settings);
-    void ReadFromSettings(QSettings * settings);
-};
-class ButtonSettingBorad : public QVBoxLayout{
-Q_OBJECT
-public:
-    class ButtonSettingPair : public QHBoxLayout{
-    public:
-        QString label_text;
-        QString prompt;
 
-        ButtonSettingPair(QWidget *parent, const QString &labelText, const QString &prompt);
 
-        ~ButtonSettingPair() override;
-
-        QLineEdit * name;
-        QLineEdit * content;
-        QPushButton * delete_button;
-    };
-    explicit ButtonSettingBorad(QWidget *parent);
-    QVBoxLayout * box;
-    QPushButton * add_button;
-    void ReloadSetting(ButtonList * list);
-
-    ~ButtonSettingBorad() override;
-public:
-signals:
-    void DeletePrompt(QString name,QString prompt);
-};
 class SettingsDialog : public QDialog
 {
     Q_OBJECT
@@ -68,7 +33,7 @@ public:
     explicit SettingsDialog(QWidget *parent = nullptr);
     ~SettingsDialog();
     QSettings *m_settings;
-    std::map<QString,QLineEdit*> * edit_info;
+    std::map<QString,QLineEdit*>* edit_info;
     ButtonList * button_list;
     ButtonSettingBorad * button_setting_broad;
 private:
